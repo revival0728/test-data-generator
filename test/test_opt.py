8e4a4a7d44a0204aad41e03afe95f89034c3cf68
revival0728 <revival0728@gmail.com>
2021-06-16 18:12:45 +0800

    fixed bug

--- test/test_opt.py
+++ test/test_opt.py
@@ -6,12 +6,12 @@ from .cpe import *
 
 def test_1():
     md = importlib.import_module('src.modules.opt')
-    tests = ['test_log.txt 10', 'abc.txt 10', 'abc.txt', 'test_log.txt aa']
+    tests = ['test_log.tdg 10', 'abc.txt 10', 'abc.txt', 'test_log.tdg aa']
     answer = [
-        {'filename': 'test_log.txt', 'nftd': 10, 'error': None},
+        {'filename': 'test_log.tdg', 'nftd': 10, 'error': None},
         {'filename': '', 'nftd': 0, 'error': 'File not found (no file abc.txt)'},
         {'filename': '', 'nftd': 0, 'error': 'Number of arguments error (except 2 get 1)'},
-        {'filename': 'test_log.txt', 'nftd': 0, 'error': 'Argument 2 must be positive integer (get aa)'}
+        {'filename': 'test_log.tdg', 'nftd': 0, 'error': 'Argument 2 must be positive integer (get aa)'}
     ]
     ret = []
     for i in tests:
