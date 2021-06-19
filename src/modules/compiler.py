857e270589af7a4bd5d06e1ced2dcd635b03ea64
revival0728 <revival0728@gmail.com>
2021-06-19 16:19:53 +0800

    finished process_single_type

--- src/modules/compiler.py
+++ src/modules/compiler.py
@@ -1,7 +1,8 @@
 class compiler:
     def __init__(self):
         self.sp_symbol = [('[', ']'), ('{', '}'), ('(', ')'), ('begin', 'end')]
-        self.sp_word = ['set', 'int', 'float', 'list', '/', 'main', 'nftd', 'fnp', 'as']
+        self.sp_word = ['set', 'int', 'float', 'list', '/', 'main', 'nftd', 'fnp', 'as', 'char']
+        self.types = ('int', 'char', 'float')
         self.sp_var = ['ENDL']
         self.ret = {
             'nftd': '',
@@ -10,18 +11,191 @@ class compiler:
             'exec': []  # ('format_string', (types), (ranges), times)
         }
         self.flag = 0
-        self.var_name = []
-        self.var = []
+        self.var = {}   # {'type', 'element'}
+
+    def _is_int_(self, s) -> bool:
+        try:
+            int(s)
+            return True
+        except ValueError:
+            return False
+
+    def _is_float_(self, s) -> bool:
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
+
+    def _is_char_(self, s) -> bool:
+        try:
+            chr(int(s))
+            return True
+        except ValueError:
+            return False
+
+    def find_quote(self, s):
+        pos = {
+            '[]': [],
+            '{}': [],
+            '()': [],
+        }
+        def _find_quote_(q, s):
+            pre = []
+            cnt = 0
+            for i in range(len(s)):
+                if s[i] == q[0]:
+                    pre.append(i)
+                    cnt += 1
+                elif s[i] == q[1]:
+                    if len(pre) == 0:
+                        self.ret['error'] = 'Sytanx error (quote {} expect {} but exceed)'.format(q[1], cnt)
+                        return 'get_error'
+                    pos[q].append((pre[-1], i))
+                    pre.pop()
+            if not len(pre) == 0:
+                self.ret['error'] = 'Sytanx error (quote {} expect {} get {})'.format(q[1], cnt, len(pre)-cnt)
+                return 'get_error'
+        for k in pos:
+            msg = _find_quote_(k, s)
+            if msg == 'get_error':
+                return msg, {}
+        return 'accept', pos
+
+    def process_single_type(self, words):
+        check_type = {
+            'int': self._is_int_,
+            'float': self._is_float_,
+            'char': self._is_char_,
+        }
+        to_type = {
+            'int': int,
+            'float': float,
+            'char': int,
+        }
+        vs = words[1:]
+        var_type = words[0]
+        if not var_type in self.types:
+            self.ret['error'] = 'Type error (do not has type {})'.format(var_type)
+            return 'get_error', ()
+        for var in vs:
+            var = var.strip()
+            msg, pos = self.find_quote(var)
+            if not msg == 'accept':
+                return 'get_error', ()
+            if not len(pos['[]']) == 0:
+                self.ret['error'] = 'Sytanx error (did not expect quotes [])'
+                return 'get_error', ()
+            if len(pos['{}']) == 0:
+                self.ret['error'] = 'Sytanx error (variable did not declare range)'
+                return 'get_error', ()
+            var_name = var[:pos['{}'][0][0]]
+            if var_name in self.var:
+                self.ret['error'] = 'Declaration of variable {} is ambiguous'.format(var_name)
+                return 'get_error'
+            rge, sit = [], 0
+            for bit in range(len(pos['{}'])):
+                rge.append([])
+                while pos['{}'][bit][0] < pos['()'][sit][0] and pos['()'][sit][1] < pos['{}'][bit][1]:
+                    sub = var[pos['()'][sit][0]+1 : pos['()'][sit][1]]
+                    if not check_type[var_type](sub):
+                        self.ret['error'] = 'Value error (cannot covert {} to integer)'.format(sub)
+                        return 'get_error', ()
+                    sub = to_type[var_type](sub)
+                    if var[pos['()'][sit][0]-1] == '/':
+                        if len(rge[-1]) < 2:
+                            self.ret['error'] = 'Sytanx error (has not declare variable {} range)'.format(var_name)
+                            return 'get_error', ()
+                        else:
+                            rge[-1].append(sub)
+                    else:
+                        rge[-1].append(sub)
+                    sit += 1
+                    if not sit < len(pos['()']):
+                        break
+            for i in range(len(rge)):
+                if len(rge[i]) < 2:
+                    self.ret['error'] = 'Sytanx error (variable {} did not has right range)'.format(var_name)
+                    return 'get_error', ()
+                rge[i] = tuple(rge[i])
+            rge = tuple(rge)
+            self.var[var_name] = {
+                'range': rge,  # ((first, last, exceptions...), ...)
+                'len': None,
+                'type': var_type,
+                'ele_type': None,
+            }
+        return 'accept', ()
+
+    def process_list(self, words):
+        check_type = {
+            'int': self._is_int_,
+            'float': self._is_float_,
+            'char': self._is_char_,
+        }
+        to_type = {
+            'int': int,
+            'float': float,
+            'char': int,
+        }
+        vs = words[1:]
+        for var in vs:
+            var = var.strip()
+            pos = self.find_quote(var)
+            if not len(pos['[]']) == 0:
+                self.ret['error'] = 'Sytanx error (did not expect quotes [])'
+                return 'get_error', ()
+            var_name = var[:pos['()'][0][0]]    # list V(int)(100){ranges...}
+            ele_type = words[pos['()'][0][0] : pos['()'][0][1]]
+            length = words[pos['()'][1][0] : pos['()'][1][1]]
+            if not self._is_int_(length):
+                self.ret['error'] = 'Value error (value of list length must be integer but get {})'.format(length)
+                return 'get_error', ()
+            if not ele_type in self.types:
+                self.ret['error'] = 'Type error (do not have type {})'.format(ele_type)
+                return 'get_error', ()
+            rge, sit = [], 2
+            for bit in range(len(pos['{}'])):
+                rge.append([])
+                while pos['{}'][bit][0] < pos['()'][sit][0] and pos['()'][sit][1] < pos['{}'][bit][1]:
+                    sub = words[pos['()'][sit][0]+1 : pos['()'][sit][1]]
+                    if not check_type[ele_type](sub):
+                        self.ret['error'] = 'Value error (cannot covert {} to type {})'.format(sub, ele_type)
+                        return 'get_error', ()
+                    sub = (sub)
+                    if words[pos['()'][sit][0]-1] == '/':
+                        if len(rge) < 2:
+                            self.ret['error'] = 'Sytanx error (has not declare variable {} range)'.format(var_name)
+                            return 'get_error', ()
+                        else:
+                            rge[-1].append(sub)
+                    else:
+                        rge[-1].append(sub)
+                    sit += 1
+            for i in rge:
+                i = tuple(i)
+            rge = tuple(rge)
+            self.var[var_name] = {
+                'range': rge,  # ((first, last, exceptions...), ...)
+                'len': length,
+                'type': 'char',
+                'ele_type': ele_type,
+            }
+            return 'accpet', ()
 
     def process_sp_word(self, words) -> tuple:
         if words == ('begin', 'main'):
             self.flag += 1
+            return 'open_file', ()
         elif words[0] == 'begin' and words[2] == 'as':
             buf = words[1].split(',')
             for i in buf:
                 i = i.strip()
-                if not i in self.var_name:
+                if not i in self.var:
                     self.ret['error'] = 'Variable {} is not declared in this scope'.format(i)
+                    return 'get_error', ()
+                if not self.var[i]['type'] == 'list':
+                    self.ret['error'] = 'Object {} is not iterable'.format(self.var[i]['type'])
                     return 'get_error', ()
             var = words[3].split(',')
             for i in var: i = i.strip()
@@ -38,7 +212,7 @@ class compiler:
                 return 'get_error', ()
             if words[1] == 'nftd':
                 try:
-                    x = int(words[2])
+                    int(words[2])
                 except ValueError:
                     self.ret['error'] = 'Value error (cannot convert {} to positive integer)'.format(words[2])
                     return 'get_error', ()
@@ -48,10 +222,21 @@ class compiler:
                     return 'get_error', ()
             self.ret[words[1]] = words[2]
             return 'accpet', ()
+        elif words[0] in ('int', 'float', 'list', 'char'):
+            if words[0] in ('int', 'float', 'char'):
+                return self.process_single_type(words)
+            elif words[0] == 'list':
+                return self.process_list(words)
+        else:
+            self.ret['error'] = 'Sytanx error (unknown syntax)'
+            return 'get_error', ()
 
     def compile(self, code) -> dict:
         lines = code.strip().split('\n')
+        cnt_line = 0
         for line in lines:
+            cnt_line += 1
+            line = line.replace(', ', ',').replace(' ,', ',')
             words = line.split()
             for i in words: i = i.strip()
             cmd, local_var = None, None
