48cc5b590294c05a3bb94f562c0c1012860cb9c9
revival0728 <revival0728@gmail.com>
2023-04-21 00:20:15 +0800

    fixed buffer_reader::read_all() test failed

--- src/exec/mod.rs
+++ src/exec/mod.rs
@@ -19,7 +19,7 @@ mod test_buffer_reader {
             Err(_) => { panic!("BufferReader::read_to_end()") }
         };
         let mut ans: Vec<String> = Vec::new();
-        let mut _ans: Vec<&str> = vec!["REP", "1", "OUT", "NEWLINE", "OUT", "RD", "QU", "8", "EC", "*", "RDS", "ABCDEFGHIJKLMNOPQRSTUVWXYZhijklmnopqrstuvwxyz~`!@#$%^&*()_-+=|[]{}:;'\"/?.>,<", "CRDS", "CRD", "OUT", "NEWLINE", "OUT", "RD", "QU", "11", "EC", "SPACE", "RDI", "1", "24", "RDI", "76", "100", "CRD", "OUT", "NEWLINE", "OUT", "RD", "QU", "15", "EC", "SPACE", "RDF", "1", "24.999", "0.001", "RDF", "75.001", "100", "0.001", "CRD", "OUT", "NEWLINE", "CREP"];
+        let mut _ans: Vec<&str> = vec!["REP", "1", "OUT", "RD", "QU", "8", "EC", "*", "RDS", "ABCDEFGHIJKLMNOPQRSTUVWXYZhijklmnopqrstuvwxyz~`!@#$%^&*()_-+=|[]{}:;'\"/?.>,<", "CRDS", "CRD", "OUT", "NEWLINE", "OUT", "RD", "QU", "11", "EC", "SPACE", "RDI", "1", "24", "RDI", "76", "100", "CRD", "OUT", "NEWLINE", "OUT", "RD", "QU", "15", "EC", "SPACE", "RDF", "1", "24.999", "0.00010000000000000005", "RDF", "75.001", "100", "0.00010000000000000005", "CRD", "CREP", "OUT", "NEWLINE"];
 
         ans.extend(_ans.iter().map(| a | -> String { a.to_string() }));
 
