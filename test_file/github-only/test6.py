a12bfe1a8e7f8d4aac2261731a9abeb847f56b8c
revival0728 <revival0728@gmail.com>
2023-04-21 16:28:25 +0800

    update cargo release setting

--- test_file/github-only/test6.py
+++ test_file/github-only/test6.py
@@ -1,9 +1,3 @@
-import sys, errno
-
-try:
-    N = int(input())
-    for _ in range(N):
-        print(input()[::-1])
-except IOError as e:
-    if e.errno == errno.EPIPE:
-        sys.exit(0)
+N = int(input())
+for _ in range(N):
+    print(input()[::-1])
